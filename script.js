a3a00f8dee7e4aabec592a366daa70a8934ea949
santiagozxo <gabriellsantiago960@gmail.com>
2025-07-13 04:31:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function rolarParaProdutos() {
+  const produtos = document.getElementById('produtos');
+  produtos.scrollIntoView({ behavior: 'smooth' });
+}
+function rolarParaProdutos() {
+  const produtos = document.getElementById('produtos');
+  produtos.scrollIntoView({ behavior: 'smooth' });
+}
